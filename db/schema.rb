876a75f175e2b501fea666804e63d206b98ebfb0
Dawna Ara <dawnaara@gmail.com>
2016-10-30 13:38:28 -0700

    dropped taggings and tags from database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161028193616) do
+ActiveRecord::Schema.define(version: 20161030203704) do
 
   create_table "directions", force: :cascade do |t|
     t.text     "step"
---
+++ test/models/tagging_model_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class TaggingModelTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
