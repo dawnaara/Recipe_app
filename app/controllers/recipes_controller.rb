1236334373efe1c766c47fc72486c10a74bc5a40
Dawna Ara <dawnaara@gmail.com>
2016-11-25 14:34:55 -0800

    returned to no tagging and search

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,9 +9,8 @@ class RecipesController < ApplicationController
       general_recipes = Recipe.search(params[:search])
       direction_recipes = Direction.search(params[:search]).map(&:recipe)
       ingredient_recipes = Ingredient.search(params[:search]).map(&:recipe)
-      tag_recipes = Tag.search(params[:search])
 
-      @recipes = (general_recipes + direction_recipes + ingredient_recipes + tag_recipes).uniq.sort {|a,b| b[:created_at] <=> a[:created_at] }.paginate(page: params[:page], per_page: 6)
+      @recipes = (general_recipes + direction_recipes + ingredient_recipes).uniq.sort {|a,b| b[:created_at] <=> a[:created_at] }.paginate(page: params[:page], per_page: 6)
     else
       @recipes = Recipe.all.order("created_at DESC").paginate(page: params[:page], per_page: 6)#calling on will_paginate gem to organize recipes 6 per page
     end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -8,9 +8,4 @@ class Tag < ActiveRecord::Base
 			Tag.find_or_create_by(name: tag.strip)
 		end
 	end
-	
-	def self.search(search)
-  		where("name ILIKE ?", "%#{search}%")
-	end
-
 end
